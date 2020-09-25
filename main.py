7c57d29f64930c7cae51e6b3c23af858869d552f
Michel Koyang-Kuleshov <michelkoyangkuleshov@gmail.com>
2020-09-26 01:10:55 +0300

    Add follow to setting page

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 SEARCHERS_PATH = 'searchers.csv'
 START_URL = 'https://analytics.google.com'
@@ -45,6 +48,19 @@ def read_params():
             namespace.silent)
 
 
+def wait_class(c_name="ga-footer-links"):
+    try:
+        WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located(
+                (By.CLASS_NAME, c_name)
+            )
+        )
+    except Exception:
+        pass
+    finally:
+        pass
+
+
 def fill_google_analytics(login, passwd, account):
     driver.get(START_URL)
     login_elem = driver.find_element_by_id(LOGIN_ID)
@@ -53,19 +69,51 @@ def fill_google_analytics(login, passwd, account):
     sleep(randint(1, 3))
     login_elem.send_keys(Keys.RETURN)
     sleep(3)
-    passwd_elem = driver.find_element_by_xpath('//input[@type="password"][@name="password"]')
+    # wait_class()
+    passwd_elem = driver.find_element_by_xpath(
+        '//input[@type="password"][@name="password"]'
+    )
     passwd_elem.click()
     passwd_elem.send_keys(passwd)
     sleep(randint(1, 3))
     passwd_elem.send_keys(Keys.RETURN)
-    home_url = 'https://analytics.google.com/analytics/web/#/report-home/a179018364w247575293p229801232'
-    admin_url = 'https://analytics.google.com/analytics/web/#/a179018364w247575293p229801232/admin'
-    spam = input('Waiting...')
+    wait_class()
+    search_res = '/admin/trackingorgainc-search-sources/'
+    admin_url = driver.current_url.replace('report-home/', '') + search_res
+    sleep(3)
+    driver.get(admin_url)
+    wait_class('ID-adminTable')
+    # print(driver.find_element_by_xpath('//div[@class="ID-adminTable"'))
     try:
         with open(SEARCHERS_PATH, 'r') as r_file:
             dict_reader = csv.DictReader(r_file, delimiter=',')
-            for row in dict_reader:
-                print(row.items())
+            for count, row in enumerate(dict_reader, 1):
+                # ID-shortcutAction ACTION-action TARGET-addOrganicSearchClicked _GAYe _GAgib _GAy
+                print(f'[{count}:] {row["Domain"]}?{row["Parameter"]}')
+                sleep(5)
+                button = driver.find_element_by_xpath(
+                    # '//input[@type="submit"]'
+                    # '//input[@class="ID-shortcutAction"]'
+                    '//div[@id="ID-m-content-content-tableControl"]'
+                )
+                # for i, e in enumerate(button):
+                #     print(f'{i} - {e}')
+                button.click()
+                wait_class('W_DECORATE_ELEMENT')
+                domain = driver.find_element_by_xpath(
+                    '//input[@data-name="domainName"]')
+                domain.click()
+                domain.send_keys(row['Domain'])
+                # W_DECORATE_ELEMENT _GASh _GAepb _GAmc-_GAxD-_GAmc
+                param = driver.find_element_by_xpath(
+                    '//input[@data-name="queryParam"]')
+                param.click()
+                param.send_keys(row['Parameter'])
+                spam = input('Waiting...')
+                button = driver.find_element_by_xpath(
+                    '//button[@type="submit"]')
+                button.click()
+                wait_class()
     except OSError as err:
         print('Ошибка чтения файла: ', err)
     else:
