957706935d3a7ad271cd4999b072768c91fc508d
Michel Koyang-Kuleshov <michel@koyang-kuleshov.ru>
2020-09-26 18:24:39 +0300

    Script for semi-automatic filling Tracing Regular Searchers

--- main.py
+++ main.py
@@ -1,144 +1,36 @@
+import sys
 import csv
-import argparse
-from random import randint
 from time import sleep
 
-import mouse
-
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import keyboard
 
 SEARCHERS_PATH = 'searchers.csv'
-START_URL = 'https://analytics.google.com'
-LOGIN_ID = 'identifierId'
 
 
-def read_params():
-    parser = argparse.ArgumentParser(description='Filling regular search result'
-                                     ' in Google Analytics')
-    parser.add_argument(
-        'login',
-        type=str,
-        default=None,
-        help='Google Analytics login'
-    )
-    parser.add_argument(
-        'password',
-        type=str,
-        default=None,
-        help='Google Analytics login'
-    )
-    parser.add_argument(
-        '-a',
-        '--account',
-        type=str,
-        default=None,
-        help='Specify the account to fill in'
-    )
-    parser.add_argument(
-        '-s',
-        '--silent',
-        type=bool,
-        default=True,
-        help='Specify False to see the filling process'
-    )
-    namespace = parser.parse_args()
-    return (namespace.login, namespace.password, namespace.account,
-            namespace.silent)
-
-
-def wait_class(c_name="ga-footer-links"):
-    try:
-        WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located(
-                (By.CLASS_NAME, c_name)
-            )
-        )
-    except Exception:
-        pass
-    finally:
-        pass
-
-
-def fill_google_analytics(login, passwd, account):
-    driver.get(START_URL)
-    driver.maximize_window()
-    sleep(3)
-    login_elem = driver.find_element_by_id(LOGIN_ID)
-    login_elem.click()
-    login_elem.send_keys(login)
-    sleep(randint(1, 3))
-    login_elem.send_keys(Keys.RETURN)
-    sleep(3)
-    # wait_class()
-    passwd_elem = driver.find_element_by_xpath(
-        '//input[@type="password"][@name="password"]'
-    )
-    passwd_elem.click()
-    passwd_elem.send_keys(passwd)
-    sleep(randint(1, 3))
-    passwd_elem.send_keys(Keys.RETURN)
-    wait_class()
-    search_res = '/admin/trackingorgainc-search-sources/'
-    admin_url = driver.current_url.replace('report-home/', '') + search_res
-    sleep(3)
-    driver.get(admin_url)
-    wait_class('ID-adminTable')
-    try:
-        with open(SEARCHERS_PATH, 'r') as r_file:
-            dict_reader = csv.DictReader(r_file, delimiter=',')
-            for count, row in enumerate(dict_reader, 1):
-                # ID-shortcutAction ACTION-action TARGET-addOrganicSearchClicked _GAYe _GAgib _GAy
-                print(f'[{count}:] {row["Domain"]}?{row["Parameter"]}')
-                sleep(3)
-                action = webdriver.ActionChains(driver)
-                button = driver.find_element_by_tag_name('body')
-                mouse.move(2244, 582, absolute=True, duration=1)
-                # mouse.click('left')
-                # print(button)
-                # action.move_by_offset(508, 373)
-                action.click()
-                action.perform()
-                spam = input('Waiting...')
-                # button.click()
-                wait_class('W_DECORATE_ELEMENT')
-                domain = driver.find_element_by_xpath(
-                    '//input[@data-name="domainName"]')
-                domain.click()
-                domain.send_keys(row['Domain'])
-                # W_DECORATE_ELEMENT _GASh _GAepb _GAmc-_GAxD-_GAmc
-                param = driver.find_element_by_xpath(
-                    '//input[@data-name="queryParam"]')
-                param.click()
-                param.send_keys(row['Parameter'])
-                spam = input('Waiting...')
-                button = driver.find_element_by_xpath(
-                    '//button[@type="submit"]')
-                button.click()
-                wait_class()
-    except OSError as err:
-        print('Ошибка чтения файла: ', err)
-    else:
-        pass
-    driver.quit()
+def fill_google_analytics():
+    with open(SEARCHERS_PATH, 'r') as r_file:
+        dict_reader = csv.DictReader(r_file, delimiter=',')
+        for count, row in enumerate(dict_reader, 1):
+            print(f'[{count}] {row["Domain"]}?{row["Parameter"]}')
+            while True:
+                if keyboard.is_pressed('ctrl'):
+                    sleep(0.3)
+                    keyboard.write(row['Domain'], delay=0)
+                    keyboard.send('Tab')
+                    try:
+                        keyboard.write(row['Parameter'], delay=0.1)
+                    except StopIteration:
+                        print(f'Допиши параметр руками: {row["Parameter"]}')
+                    keyboard.send('Tab')
+                    keyboard.send('Tab')
+                    keyboard.send('Return')
+                    break
+                if keyboard.is_pressed('esc'):
+                    sys.exit(1)
     print('Task done')
 
 
 if __name__ == "__main__":
-    login, password, account, silent = read_params()
-    # TODO: Delete row below
-    silent = False
-    options = None
-    if silent:
-        options = Options()
-        options.add_argument('--headless')
-    driver = webdriver.Firefox(
-        executable_path='/usr/bin/geckodriver',
-        firefox_binary='/usr/bin/firefox',
-        options=options
-    )
-    fill_google_analytics(login, password, account)
+    print('Для заполнения поля Домен и Параметр нажми CTRL'
+          '\nДля остановки скрипта ESC')
+    fill_google_analytics()
