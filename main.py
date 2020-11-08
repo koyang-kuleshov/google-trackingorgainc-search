32512bc8e5f8c9881a64d64d556c71b26f5050a6
Michel Koyang-Kuleshov <michel@koyang-kuleshov.ru>
2020-11-08 18:38:50 +0300

    Lint main.py by flake8 wemake-python-styleguide

--- main.py
+++ main.py
@@ -1,71 +1,76 @@
-"""Fill Google Analytics"""
-import sys
-from collections import namedtuple
+"""Filling Google Analytics."""
 import csv
-from time import sleep, monotonic
+from collections import namedtuple
+from time import monotonic, sleep
 
-from pynput import keyboard
 import pyautogui
-import pyscreenshot as ImageGrab
-
+import pyscreenshot as imagegrab
+from pynput import keyboard
 
 SEARCHERS_PATH = 'searchers.csv'
 CatchingObj = namedtuple('CatchingObj', ['bbox', 'color'])
 GREY_BACKGROUND = CatchingObj((2120, 487, 2121, 488), (204, 204, 204))
 BLUE_BUTTON = CatchingObj((2120, 760, 2121, 761), (74, 139, 245))
 DELAY = 0.3
+ADD_BUTTON = 2250, 510
 
 
-def wait_object(obj):
-    """Function waits until the object appears on the screen"""
+def wait_object(expected_obj):
+    """Wait for an object on the screen.
 
+    Args:
+        expected_obj: namedtuple with coordinates and color of expected object.
+    """
     while True:
         sleep(DELAY)
-        image = ImageGrab.grab(bbox=obj.bbox)
+        image = imagegrab.grab(bbox=expected_obj.bbox)
         tmp_color = image.convert('RGB').getcolors()[0][1]
 
-        if tmp_color == obj.color:
+        if tmp_color == expected_obj.color:
             break
 
 
+def press_tab(keybrd, counter):
+    """Press tab couner times.
+
+    Args:
+        keybrd: keyboard object.
+        counter: number of times to press.
+    """
+    for _ in range(counter):
+        keybrd.press(keyboard.Key.tab)
+        keybrd.release(keyboard.Key.tab)
+
+
 def fill_google_analytics():
-    """Fill Regular Search information"""
-    start_time = monotonic()
+    """Fill Regular Search information."""
     keybrd = keyboard.Controller()
     with open(SEARCHERS_PATH, 'r') as r_file:
         dict_reader = csv.DictReader(r_file, delimiter=',')
-
         for count, row in enumerate(dict_reader, 1):
             print(f'[{count}] {row["Domain"]}?{row["Parameter"]}')
-            pyautogui.click(2250, 510)
+            pyautogui.click(ADD_BUTTON)
             wait_object(BLUE_BUTTON)
             sleep(DELAY)
-            keybrd.press(keyboard.Key.tab)
-            keybrd.release(keyboard.Key.tab)
-            keybrd.press(keyboard.Key.tab)
-            keybrd.release(keyboard.Key.tab)
+            press_tab(keybrd, 2)
             keybrd.type(row['Domain'])
-            keybrd.press(keyboard.Key.tab)
-            keybrd.release(keyboard.Key.tab)
+            press_tab(keybrd, 1)
             keybrd.type(row.get('Parameter'))
-            keybrd.press(keyboard.Key.tab)
-            keybrd.release(keyboard.Key.tab)
-            keybrd.press(keyboard.Key.tab)
-            keybrd.release(keyboard.Key.tab)
+            press_tab(keybrd, 2)
             keybrd.press(keyboard.Key.enter)
             keybrd.release(keyboard.Key.enter)
             sleep(DELAY)
             wait_object(GREY_BACKGROUND)
-    end_time = monotonic() - start_time
-    print(f'Task done. Time: {end_time}')
-    sys.exit(1)
 
 
-if __name__ == "__main__":
-    print('1. Запустить скрипт',
+if __name__ == '__main__':
+    print('1. Запустить скрипт\n',
           '2. Перейти на страницу "Источники обычных результатов поиска"',
-          ' в Google Analytics'
+          ' в Google Analytics\n',
           '3. Программа автоматически заполнит все параметры из файла'
           ' searchers.csv',
-          sep='\n')
+          )
+    start_time = monotonic()
     fill_google_analytics()
+    end_time = monotonic() - start_time
+    print(f'Task done. Time: {end_time}')
